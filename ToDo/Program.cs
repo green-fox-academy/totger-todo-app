5ee2b2a84c5362d21b38ee8b2db343ff5cd87e0a
Gergo Toth <toothgergo@gmail.com>
2017-09-28 11:42:37 +0200

    feat(ToDo): welcome screen listing possible arguments added.

---
+++ ToDo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ToDo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WriteArguments();
+        }
+
+        private static void WriteArguments()
+        {
+            string text = System.IO.File.ReadAllText(@"../../assets/arguments.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
